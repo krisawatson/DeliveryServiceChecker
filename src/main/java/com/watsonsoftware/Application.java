3a1e94b157bb6559c4ea92cf8cff2d7d215a51b7
Kris Watson <kris.watson@jajazz.co.uk>
2020-03-31 20:29:41 +0100

    Adding argument so can set the rate in which to call the endpoint.

--- src/main/java/com/watsonsoftware/Application.java
+++ src/main/java/com/watsonsoftware/Application.java
@@ -3,21 +3,24 @@ package com.watsonsoftware;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Timer;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
-import static java.util.concurrent.TimeUnit.MINUTES;
 
 @Slf4j
 public class Application {
 
-    private static final long TIMER_RATE = 2 * 60 * 1000;
+    private static final int DEFAULT_RATE = 2;
+    private static final long MINUTE = 60 * 1000;
 
     public static void main(String[] args) {
         log.info("Starting application");
+        int rate = DEFAULT_RATE;
+        if (args.length > 0) {
+            log.info("Args are " + String.join(", ", args));
+            rate = Integer.parseInt(args[0]);
+        }
+        log.info("Time is set to " + rate);
         CheckAsdaDeliveryTask task = new CheckAsdaDeliveryTask();
         try {
-            new Timer().scheduleAtFixedRate(task, 0, TIMER_RATE);
+            new Timer().scheduleAtFixedRate(task, 0, rate * MINUTE);
         } catch (Exception e) {
             log.error("Error while running scheduled task", e);
         }
